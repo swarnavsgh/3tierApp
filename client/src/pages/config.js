133813ccc94770de9a6f6e87bf790ed1058e9d8e
devopsgithub <swarna.sfcrm@gmail.com>
2025-01-31 00:40:38 -0500

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,3 @@
-// const API_BASE_URL = "http://localhost:8800";
+// const API_BASE_URL = "http://localhost:8800"; OR Use Backend PublicIP, EX: "http://http://3.85.34.97/"
 const API_BASE_URL = "http://app.trikuta.site";
 export default API_BASE_URL;
